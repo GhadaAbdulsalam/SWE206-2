830cfbec41419dc73cb308a0fe77780b187fb683
GHADA ABDULSALAM <g202212880@kfupm.edu.sa>
2024-02-07 15:20:08 +0300

    add print statment

--- App.java
+++ App.java
@@ -10,6 +10,7 @@ public class App {
         System.out.println("bye, buddy!");
 
         System.out.println(generateRandomGroups(readFile("Student List.txt"),4));
+        System.out.println("done");
 
     }
 
